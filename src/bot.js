021bff40a63a017c4750f9990ce0ef7f1c092318
otavio.lube <otavio.santos@uvv.br>
2024-03-26 08:12:46 -0300

    Adicionei o node telegram bot

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,17 @@
-console.log('teste');
+const TelegramBot = require("node-telegram-bot-api");
+const token = "YOUR_TELEGRAM_BOT_TOKEN";
 
-function soma(a,b) {
-    return a + b;
-}
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match[1];
+
+  bot.sendMessage(chatId, resp);
+});
+
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, "Received your message");
+});
